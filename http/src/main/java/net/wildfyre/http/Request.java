f7a0ba1a056ec237a91e1d11d2148195dbd79719
Ivan Canet <ivan.canet@gmail.com>
2018-07-12 10:31:34 +0200

    Removed references to internal classes & removed unneeded Connection class
    Summary:
     - Removed class Connection
     - Fixing inconsistent identation
    
    Test Plan: Run the tests & check that everything still works with no warnings
    
    Reviewers: WyldBot, Info-Screen
    
    Reviewed By: WyldBot, Info-Screen
    
    Maniphest Tasks: T231
    
    Differential Revision: https://phabricator.wildfyre.net/D176

--- http/src/main/java/net/wildfyre/http/Request.java
+++ http/src/main/java/net/wildfyre/http/Request.java
@@ -1,104 +1,191 @@
 package net.wildfyre.http;
 
+import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
  * Wrapper around the HTTP requests.
- *
+ * <p>
  * This class is NOT part of the public API.
  */
 public class Request {
 
     //region Select the URL of the server depending on the building environment
-	private static final String API_URL = "https://api.wildfyre.net";
-	private static final String API_URL_TESTING = "http://localhost:8000";
-	static final boolean isTesting = !System.getProperty("org.gradle.test.worker", "default").equals("default");
-	static String getURL(){return isTesting ? API_URL_TESTING : API_URL;}
+    private static final String API_URL = "https://api.wildfyre.net";
+    private static final String API_URL_TESTING = "http://localhost:8000";
+    static final boolean isTesting = !System.getProperty("org.gradle.test.worker", "default").equals("default");
+    static String getURL(){return isTesting ? API_URL_TESTING : API_URL;}
     //endregion
+    //region Variables
 
-	public static final String CHARSET = "UTF-8";
+    public static final String CHARSET = "UTF-8";
 
-	/**
-	 * Sends a request to the API.
-	 * @param method the HTTP method used by the request.
-	 * @param token the token used to authenticate the user (can be {@code null} for un-authenticated requests).
-	 * @param address the address you want to query (eg. /account/)
-	 * @param params the parameters you provide
-	 * @return The JSON response from the server.
-	 */
-	protected static JsonValue request(Method method, String token, String address, JsonObject params)
-		throws CantConnectException, IssueInTransferException {
-		try {
+    //endregion
+    //region Requests
 
-			// Creation of the URL
-			URL url = new URL(getURL() + address);
-			byte[] postDataBytes = convertToByteArray(params);
+    /**
+     * Sends a request to the API.
+     *
+     * @param method  the HTTP method used by the request.
+     * @param token   the token used to authenticate the user (can be {@code null} for un-authenticated requests).
+     * @param address the address you want to query (eg. /account/)
+     * @param params  the parameters you provide
+     * @return The JSON response from the server.
+     */
+    protected static JsonValue request(Method method, String token, String address, JsonObject params)
+        throws CantConnectException, IssueInTransferException {
+        try {
 
-			// Connection
-			Connection conn = connect(url);
+            // Creation of the URL
+            URL url = new URL(getURL() + address);
+            byte[] postDataBytes = convertToByteArray(params);
 
-			conn.setRequestMethod(method);
+            // Connection
+            HttpURLConnection conn = connect(url);
 
-			conn.connection.setRequestProperty("Host", url.getHost());
-			if(token != null)	conn.connection.setRequestProperty("Authorization", "token " + token);
-			conn.connection.setRequestProperty("From", "lib-java");
-			conn.connection.setRequestProperty("Content-Type", "application/json");
-			conn.connection.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-			conn.connection.setDoOutput(true);
-			conn.write(postDataBytes);
+            setRequestedMethod(conn, method);
 
-			return Connection.readJson(conn.read());
+            conn.setRequestProperty("Host", url.getHost());
+            if (token != null) conn.setRequestProperty("Authorization", "token " + token);
+            conn.setRequestProperty("From", "lib-java");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            write(conn, postDataBytes);
 
-		} catch (MalformedURLException e) {
-			throw new IllegalArgumentException("The provided address " + address + " at " + getURL() + " is malformed: "
-					+ getURL()+address, e);
-		}
-	}
+            return read(getInputStream(conn));
 
-	/**
-	 * Connects to the provided URL.
-	 * @param url the URL the API should connect to
-	 * @return The connection to the server, on success.
-	 * @throws CantConnectException Failure to connect to the server.
-	 */
-	static Connection connect(URL url) throws CantConnectException {
-		try {
-			return new Connection(url.openConnection());
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("The provided address " + address + " at " + getURL() + " is malformed: "
+                + getURL() + address, e);
+        }
+    }
 
-		} catch (IOException e) {
-			throw new CantConnectException("Cannot connect to the server.", e);
-		}
-	}
+    //endregion
+    //region Helpers
 
-	/**
-	 * Converts the JSON parameters to an array of bytes that can be sent in the request. The charset used is specified
-	 * in {@link #CHARSET} ({@value #CHARSET}).
-	 * @param params the JSON parameters to be converted.
-	 * @return A byte array representing the provided parameters.
-	 */
-	static byte[] convertToByteArray(JsonValue params){
-		try {
-			return params.toString().getBytes(CHARSET);
+    /**
+     * Connects to the provided URL.
+     *
+     * @param url the URL the API should connect to
+     * @return The connection to the server, on success.
+     * @throws CantConnectException Failure to connect to the server.
+     */
+    static HttpURLConnection connect(URL url) throws CantConnectException {
+        try {
+            return (HttpURLConnection) url.openConnection();
 
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException("There was a problem with the character encoding '"+CHARSET+"'. Because it is" +
-				"hard-written in the class, this error should never happen.", e);
-		}
-	}
+        } catch (IOException e) {
+            throw new CantConnectException("Cannot connect to the server.", e);
+        }
+    }
 
-	/**
-	 * Thrown when the request failed because the requested could not connect to the server.
-	 */
-	public static class CantConnectException extends IOException {
-		CantConnectException(String msg, Exception e){
-			super(msg, e);
-		}
-	}
+    /**
+     * Converts the JSON parameters to an array of bytes that can be sent in the request. The charset used is specified
+     * in {@link #CHARSET} ({@value #CHARSET}).
+     *
+     * @param params the JSON parameters to be converted.
+     * @return A byte array representing the provided parameters.
+     */
+    static byte[] convertToByteArray(JsonValue params) {
+        try {
+            return params.toString().getBytes(CHARSET);
 
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("There was a problem with the character encoding '" + CHARSET + "'. Because it is" +
+                "hard-written in the class, this error should never happen.", e);
+        }
+    }
+
+    /**
+     * Sets the method for the request and handles eventual exceptions.
+     * @param connection the connection that should be modified
+     * @param method the HTTP method used for the request
+     */
+    static void setRequestedMethod(HttpURLConnection connection, Method method) {
+        try {
+            connection.setRequestMethod(method.toString());
+
+        } catch (ProtocolException e) {
+            throw new IllegalArgumentException("Cannot set the method to " + method, e);
+        }
+    }
+
+    /**
+     * Writes the bytes to the connection.
+     * @param connection the connection
+     * @param bytes the data that should be sent
+     * @throws CantConnectException if the output stream of the connection is unreachable for some reason
+     */
+    static void write(HttpURLConnection connection, byte[] bytes) throws CantConnectException {
+        try {
+            connection.getOutputStream().write(bytes);
+
+        } catch (IOException e) {
+            throw new CantConnectException("Cannot get output stream to the connection.", e);
+        }
+    }
+
+    /**
+     * Reads the server's response and handles eventual exceptions.
+     * @return The server's response.
+     * @throws IssueInTransferException If the server refuses the request, see
+     * {@link IssueInTransferException#getJson() getJson()} to get the eventual error message.
+     */
+    static InputStream getInputStream(HttpURLConnection connection) throws IssueInTransferException {
+        try {
+            return connection.getInputStream();
+
+        } catch (IOException e) {
+            throw new IssueInTransferException("The server refused the request.", connection.getErrorStream());
+        }
+    }
+
+    /**
+     * Reads the JSON data.
+     * @param input the input
+     * @return The JSON data contained by the server's response
+     * @throws IssueInTransferException If any I/O occurs while trying to read the JSON data.
+     */
+    static JsonValue read(InputStream input) throws IssueInTransferException {
+        try {
+            return Json.parse (
+                new BufferedReader (
+                    new InputStreamReader (
+                        input,
+                        Request.CHARSET
+                    )
+                )
+            );
+
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("There was an encoding error. Since the encoding is hardcoded in Request.CHARSET," +
+                "this error should never occur. Please report to the developers with the full stacktrace.", e);
+
+        } catch (IOException e) {
+            throw new IssueInTransferException("There was an I/O error while parsing the JSON data, or the server " +
+                "refused the request.", e);
+        }
+    }
+
+    //endregion
+    //region Exceptions
+
+    /**
+     * Thrown when the request failed because the requested could not connect to the server.
+     */
+    public static class CantConnectException extends IOException {
+        CantConnectException(String msg, Exception e) {
+            super(msg, e);
+        }
+    }
+
+    //endregion
 }
--- http/src/test/java/net/wildfyre/http/RequestTest.java
+++ http/src/test/java/net/wildfyre/http/RequestTest.java
@@ -4,33 +4,56 @@ import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 import static com.eclipsesource.json.WriterConfig.PRETTY_PRINT;
 import static net.wildfyre.http.Method.POST;
 import static org.junit.Assert.*;
 
 public class RequestTest {
 
-	@Test
-	public void request(){
-		try {
-			JsonValue j = Request.request(POST, null, "/account/auth/", new JsonObject()
-				.add("username", "libtester")
-				.add("password", "thisisnotatest"));
+    @Test
+    public void request(){
+        try {
+            JsonValue j = Request.request(POST, null, "/account/auth/", new JsonObject()
+                .add("username", "libtester")
+                .add("password", "thisisnotatest"));
 
             assertNotNull(j.asObject().getString("token", null));
-		} catch (Request.CantConnectException e) {
-			throw new IllegalArgumentException(e);
-		} catch (IssueInTransferException e) {
-			if(e.getJson().isPresent())
-				fail("Issue in transfer: " + e.getJson().get().toString(PRETTY_PRINT));
-			else
-				fail("Unknown issue in transfer.");
-		}
-	}
+        } catch (Request.CantConnectException e) {
+            throw new IllegalArgumentException(e);
+        } catch (IssueInTransferException e) {
+            if(e.getJson().isPresent())
+                fail("Issue in transfer: " + e.getJson().get().toString(PRETTY_PRINT));
+            else
+                fail("Unknown issue in transfer.");
+        }
+    }
 
-	@Test
-	public void testAvailabilityOfDatabase(){
-	    assertNotEquals("default", System.getProperty("org.gradle.test.worker", "default"));
+    @Test
+    public void testJSON() {
+        InputStream input = null;
+        try {
+            input = new ByteArrayInputStream("{\"data\": 2}".getBytes(Request.CHARSET));
+        } catch (UnsupportedEncodingException e) {
+            fail("Could not read Stream.");
+        }
+
+        assertNotNull(input);
+        try {
+            JsonValue json = Request.read(input);
+
+            assertEquals(new JsonObject().add("data", 2), json);
+        } catch (IssueInTransferException e) {
+            fail("Could not parse to JSON.");
+        }
+    }
+
+    @Test
+    public void testAvailabilityOfDatabase(){
+        assertNotEquals("default", System.getProperty("org.gradle.test.worker", "default"));
         assertTrue(Request.isTesting);
     }
 
